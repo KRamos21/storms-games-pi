ad24d2bc19f85364e2c0c5b8884e014cf05f5490
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2021-09-01 14:31:48 -0300

    Criei a rota "seguranca"

--- routes/seguranca.js
+++ routes/seguranca.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("conta", {title: "Express"});
+  res.render("seguranca");
 });
 
 module.exports = router;
