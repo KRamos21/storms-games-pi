1f949eea2a3d767a5aba78536b23b24c1d9c46c0
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2021-09-02 15:11:47 -0300

    Criei a rota "cadastroUsuario"

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const bibliotecaRouter = require("./routes/biblioteca");
+const cadastroUsuarioRouter = require("./routes/cadastroUsuario");
 const contaRouter = require("./routes/contas");
-const segurancaRouter = require("./routes/seguranca");
-const transacoesRouter = require("./routes/transacoes");
-const metodosPagamentoRouter = require("./routes/metodosPagamento");
-const pedidosRouter = require("./routes/pedidos");
-const jogosRouter = require("./routes/jogos");
 const forumJogoRouter = require("./routes/forumJogo");
 const forunsRouter = require("./routes/foruns");
+const jogosRouter = require("./routes/jogos");
+const metodosPagamentoRouter = require("./routes/metodosPagamento");
+const pedidosRouter = require("./routes/pedidos");
+const segurancaRouter = require("./routes/seguranca");
 const topicoForumRouter = require("./routes/topicoForum");
-const bibliotecaRouter = require("./routes/biblioteca");
+const transacoesRouter = require("./routes/transacoes");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -30,17 +31,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/biblioteca", bibliotecaRouter);
+app.use("/cadastroUsuario", cadastroUsuarioRouter);
 app.use("/conta", contaRouter);
-app.use("/seguranca", segurancaRouter);
-app.use("/transacoes", transacoesRouter);
-app.use("/metodosPagamento", metodosPagamentoRouter);
-app.use("/pedidos", pedidosRouter);
-app.use("/jogo", jogosRouter);
 app.use("/forumJogo", forumJogoRouter);
 app.use("/foruns", forunsRouter);
+app.use("/jogo", jogosRouter);
+app.use("/metodosPagamento", metodosPagamentoRouter);
+app.use("/pedidos", pedidosRouter);
+app.use("/seguranca", segurancaRouter);
 app.use("/topico", topicoForumRouter);
-app.use("/biblioteca", bibliotecaRouter);
+app.use("/transacoes", transacoesRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
