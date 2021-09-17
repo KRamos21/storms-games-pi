93b186b31f7840eb717a8e360041b41247f393cf
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2021-09-17 19:47:39 -0300

    Criei migration da tabela users

---
+++ database/migrations/20210917222438-create_table_users.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('users', {
+    id: {
+      primaryKey: true,
+      type: Sequelize.INTEGER.UNSIGNED,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    nome: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+    sobrenome: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+    senha: {
+      type: Sequelize.STRING(20),
+      allowNull: false,
+    },
+    cpf: {
+      type: Sequelize.CHAR(11),
+      allowNull: true,
+    },
+    data_nascimento: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+    },
+    sexo: {
+      type: Sequelize.STRING(9),
+      allowNull: false,
+    },
+    telefone: {
+      type: Sequelize.STRING(15),
+      allowNull: false,
+    },
+    created_at: {
+      type: "TIMESTAMP",
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    },
+    updated_at: {
+      type: "TIMESTAMP",
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    }
+   });   
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');    
+  }
+};
