627146c0ee4de35c5d303e91ee77bbd77aa545e5
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2021-09-20 18:39:11 -0300

    Refatorei as models 'Purchase' e 'Purchase_item'

--- database/models/Purchase_item.js
+++ database/models/Purchase_item.js
@@ -1,36 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
-    const Purchase_item = sequelize.define("Purchase_item", {
-        quantidade: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        valor: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        purchase_id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNull: false,
-        },
-        game_id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNull: false,
-        },
+  const Purchase_item = sequelize.define("Purchase_item", {
+    quantidade: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    valor: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    purchase_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    game_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  });
+
+  Purchase_item.associate = (models) => {
+    Purchase_item.belongsTo(models.Purchase, {
+      foreignKey: "purchase_id",
+      as: "purchase",
     });
 
-    Purchase_item.associate = models => {
-        Purchase_item.belongsTo(models.Purchase, {
-            foreignKey: 'purchase_id',
-            as: 'purchase'
-        });
-    };
+    Purchase_item.belongsTo(models.Game, {
+      foreignKey: "game_id",
+      as: "game",
+    });
+  };
 
-    Purchase_item.associate = models => {
-        Purchase_item.belongsTo(models.Game, {
-            foreignKey: 'game_id',
-            as: 'game'
-        });
-    };
-
-    return Purchase;
+  return Purchase_item;
 };
