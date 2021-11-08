f6644fe0fa68367cd1d314d4709b9a806da2b6e3
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2021-11-08 18:52:00 -0300

    Script para renderizar o menu oculto

--- public/javascripts/pesquisaHeader.js
+++ public/javascripts/pesquisaHeader.js
@@ -43,3 +43,48 @@ async function pesquisarTitulo(input) {
     menuBar.style.borderBottomRightRadius = "5px"
   }
 };
+
+window.addEventListener("DOMContentLoaded", () => {
+  const user = document.getElementById("user");
+
+  if (window.innerWidth < 900) {
+    user.removeAttribute("href");
+    user.setAttribute("onclick", "exibirMenu(this)")
+  } else {
+    user.href = "/conta";
+    user.removeAttribute("onclick");
+  }
+});
+
+window.addEventListener("resize", () => {
+  const user = document.getElementById("user");
+
+  if (window.innerWidth < 900) {
+    user.removeAttribute("href");
+    user.setAttribute("onclick", "exibirMenu(this)")
+  } else {
+    user.href = "/conta";
+    user.removeAttribute("onclick");
+  }
+})
+
+function exibirMenu(user) {
+  const menuBar = document.getElementById("menu__searchbar");
+
+  const header = document.getElementById("header");
+  const menuOculto = document.getElementById("menu-oculto");
+
+  if (menuOculto.classList.contains("menu-oculto--hidden")) {
+    menuOculto.classList.remove("menu-oculto--hidden");
+    
+    menuOculto.style.position = "absolute";
+    menuOculto.style.zIndex = "999"
+    menuOculto.style.right = header.getBoundingClientRect().left + "px";
+    menuOculto.style.top = header.getBoundingClientRect().bottom + "px";;
+    menuOculto.style.width = (window.innerWidth / 2) + "px";
+  } else {
+    menuOculto.removeAttribute("style");
+
+    menuOculto.classList.add("menu-oculto--hidden")
+  }
+}
